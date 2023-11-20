9ddf0c609371fde75e7481b399911f091c32b1ce
나강민 <139530542+KangminNa@users.noreply.github.com>
2023-11-20 23:09:14 +0900

    Feat : passport 가져와 회원가입 및 로그인 기능 구현

--- node-birds-sns/controllers/page.js
+++ node-birds-sns/controllers/page.js
@@ -1,16 +1,15 @@
-exports.renderProfile = (req, res, next) => {
+exports.renderProfile = (req, res) => {
     res.render('profile', { title: '내 정보 - NodeBird' });
 };
 
-exports.renderJoin = (req, res, next) => {
+exports.renderJoin = (req, res) => {
     res.render('join', { title: '회원가입 - NodeBird' });
 };
 
 exports.renderMain = (req, res, next) => {
+    const twits = [];
     res.render('main', {
         title: 'NodeBird',
-        twits: [],
+        twits,
     });
 };
-
-// 라우터 -> 컨트롤러 -> 서비스(요청, 응답 모른다)
